531d330f7d5cec38ac4ec9821abcfa2e4f1c21e5
thereclaimer <sam.davenport@thereclaimergame.com>
2025-10-27 15:34:04 -0500

    treesitter and colors

--- lua/config/keybinds.lua
+++ lua/config/keybinds.lua
@@ -1,2 +1,15 @@
 vim.g.mapleader = " "
+
 vim.keymap.set("n", "<leader>cd", vim.cmd.Ex)
+
+-- window splitting
+vim.keymap.set("n", "<leader>wr", "<cmd>vsplit<CR><C-w>l")           -- right
+vim.keymap.set("n", "<leader>wl", "<cmd>leftabove vsplit<CR><C-w>h") -- left
+vim.keymap.set("n", "<leader>wd", "<cmd>split<CR><C-w>")             -- down 
+vim.keymap.set("n", "<leader>wu", "<cmd>aboveleft split<CR><C-w>k")  -- up 
+
+
+vim.keymap.set("n", "<C-d>", "<C-d>zz")              -- page down and center 
+vim.keymap.set("n", "<C-u>", "<C-u>zz")              -- page up and center
+
+vim.keymap.set("i", "<C-c>", "<Esc>")
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,3 +3,4 @@ vim.opt.cursorline = true
 vim.opt.relativenumber = true
 vim.opt.shiftwidth = 4
 vim.opt.guicursor = "n-v-c-i:block"
+vim.opt.termguicolors = true
